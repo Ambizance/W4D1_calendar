d8b510d39c95c6900356178618d36da8ede7babd
Ambizance <ambroise.breant@gmail.com>
2021-07-20 05:54:48 -0400

    Ajout de l'ensemble du dossier

--- lib/event.rb
+++ lib/event.rb
@@ -1,3 +1,22 @@
-class Event
+require 'time'
 
+class Event
+attr_accessor :start_date, :duration, :title, :attendees
+
+    def initialize(start_date, duration, title, attendees)
+
+        @start_date = Time.parse("#{start_date}")
+        @duration = duration.to_i
+        @title = title
+        @attendees = []
+        @attendees << attendees
+    end
+
+    def postpone_24
+        @start_date= @start_date+24*3600
+    end
 end
+
+binding.pry
+puts "end of file"
+
--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,19 @@
 class User
 
+    attr_accessor :age
+    attr_accessor :mail
+    #attr_accessor :mail
+    @@array_all_user = []
+
+    def initialize(mail, age)
+        @mail = mail.to_s
+        @age = age.to_i
+        @@array_all_user << self 
+    end
+
+    def self.all
+        return @@array_all_user
+    end
+
 end
+
